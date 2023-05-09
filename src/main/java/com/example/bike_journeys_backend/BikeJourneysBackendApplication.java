9abc9a5b066003873301d28d682859ec3045b0f6
hajri <c1almo00@students.oamk.fi>
2023-05-09 10:16:23 +0300

    spring boot initialized and dependencies configured

---
+++ src/main/java/com/example/bike_journeys_backend/BikeJourneysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bike_journeys_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeJourneysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeJourneysBackendApplication.class, args);
+	}
+
+}
